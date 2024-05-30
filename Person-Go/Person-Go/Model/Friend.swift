7a9022ab48a20cf1dcc5d452890c7ca173224d50
ZehuiWangTeddy <111961563+ZehuiWangTeddy@users.noreply.github.com>
2024-05-30 23:02:07 +0200

    update friend model file

--- Person-Go/Person-Go/Model/Friend.swift
+++ Person-Go/Person-Go/Model/Friend.swift
@@ -2,9 +2,22 @@ import Foundation
 
 struct Friend: Identifiable, Hashable, Decodable {
     var id: Int
-    var name: String
+//    let id = UUID()
     var friendId: UUID
-    var distance: Double // Distance in kilometers
+//    var name: String
+//    var distance: Double
+//    var avatar: String // Assume this is the name of an image in Assets.xcassets
     var totalInventory: Int
+
     var profiles: Profile
+}
+
+extension Friend {
+    func username() -> String {
+        let profile = self.profiles
+        
+        guard profile.username != nil else { return "" }
+        
+        return profile.username!
+    }
 }
